d27804e45d19cf54a74313ed69b72fe3600df3f7
vapegang <42774540+vapegang@users.noreply.github.com>
2021-01-24 17:37:04 +0400

    Add files via upload

---
+++ helloworld/src/main/java/com/mycompany/helloworld/Main.java
@@ -0,0 +1,8 @@
+package com.mycompany.helloworld;
+
+public class Main {
+    public static void main(String[]args){
+            { System.out.println("Hello, my name is Daniil. I'm from Russia!");}
+    }
+    }
+
